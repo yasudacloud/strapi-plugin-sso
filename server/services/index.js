00d1963a59e25f0da815d2dfcde64c920c967d43
yasudacloud <hello@yasuda.cloud>
2022-05-04 18:54:49 +0900

    Remove unnecessary codes

--- server/services/index.js
+++ server/services/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
-// const myService = require('./my-service');
 const oauth = require('./oauth')
 const role = require('./role')
+
 module.exports = {
-  // myService,
   oauth,
   role
 };
