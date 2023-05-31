a345205c354f03f3621aff907c233469200ef379
Alexey Taranets <alexey.taranets@altexsoft.com>
2023-05-31 19:04:43 +0300

    Keycloak register controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,13 +1,15 @@
-"use strict";
+'use strict';
 
-const google = require("./google");
-const cognito = require("./cognito");
-const azuread = require("./azuread");
-const role = require("./role");
+const google = require('./google');
+const cognito = require('./cognito');
+const azuread = require('./azuread');
+const keycloak = require('./keycloak');
+const role = require('./role');
 
 module.exports = {
   google,
   cognito,
   azuread,
+  keycloak,
   role,
 };
