332e560f6ce7925bb03a5244bfc3b5a21b11dd50
minozheros <klaus.grosskopff@gmx.net>
2023-10-07 14:04:41 +0200

    add - oidc controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,11 +3,13 @@
 const google = require("./google");
 const cognito = require("./cognito");
 const azuread = require("./azuread");
+const oidc = require("./oidc");
 const role = require("./role");
 
 module.exports = {
   google,
   cognito,
   azuread,
+  oidc,
   role,
 };
