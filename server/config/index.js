c480802c48bc5520949df83a7e6f0c3f66980d17
Adam Post <32047648+adamrpostjr@users.noreply.github.com>
2024-01-08 10:13:05 -0500

    Added 'Remember Me' to config options (#40)
    * Add REMEMBER_ME configuration option
    
    * Add REMEMBER_ME configuration for SSO plugin

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,8 @@
 
 module.exports = {
   default: {
+    REMEMBER_ME: false,
+
     GOOGLE_OAUTH_REDIRECT_URI: 'http://localhost:1337/strapi-plugin-sso/google/callback',
     GOOGLE_GSUITE_HD: '',
     GOOGLE_ALIAS: '',
--- server/services/oauth.js
+++ server/services/oauth.js
@@ -1,20 +1,20 @@
-const strapiUtils = require('@strapi/utils');
-const generator = require('generate-password');
+const strapiUtils = require("@strapi/utils");
+const generator = require("generate-password");
 
-module.exports = ({strapi}) => ({
+module.exports = ({ strapi }) => ({
   async createUser(email, lastname, firstname, locale, roles = []) {
     // If the email address contains uppercase letters, convert it to lowercase and retrieve it from the DB. If not, register a new email address with a lower-case email address.
-    const userService = strapi.service('admin::user')
+    const userService = strapi.service("admin::user");
     if (/[A-Z]/.test(email)) {
-      const dbUser = await userService.findOneByEmail(email.toLocaleLowerCase())
+      const dbUser = await userService.findOneByEmail(email.toLocaleLowerCase());
       if (dbUser) {
-        return dbUser
+        return dbUser;
       }
     }
 
     const createdUser = await userService.create({
-      firstname: firstname ? firstname : 'unset',
-      lastname: lastname ? lastname : '',
+      firstname: firstname ? firstname : "unset",
+      lastname: lastname ? lastname : "",
       email: email.toLocaleLowerCase(),
       roles,
       preferedLanguage: locale,
@@ -23,57 +23,63 @@ module.exports = ({strapi}) => ({
     return await userService.register({
       registrationToken: createdUser.registrationToken,
       userInfo: {
-        firstname: firstname ? firstname : 'unset',
-        lastname: lastname ? lastname : 'user',
+        firstname: firstname ? firstname : "unset",
+        lastname: lastname ? lastname : "user",
         password: generator.generate({
           length: 43, // 256 bits (https://en.wikipedia.org/wiki/Password_strength#Random_passwords)
           numbers: true,
           lowercase: true,
           uppercase: true,
           exclude: '()+_-=}{[]|:;"/?.><,`~',
-          strict: true
+          strict: true,
         }),
-      }
+      },
     });
   },
   addGmailAlias(baseEmail, baseAlias) {
     if (!baseAlias) {
-      return baseEmail
+      return baseEmail;
     }
-    const alias = baseAlias.replace('/+/g', '')
-    const beforePosition = baseEmail.indexOf('@')
-    const origin = baseEmail.substring(0, beforePosition)
-    const domain = baseEmail.substring(beforePosition)
-    return `${origin}+${alias}${domain}`
+    const alias = baseAlias.replace("/+/g", "");
+    const beforePosition = baseEmail.indexOf("@");
+    const origin = baseEmail.substring(0, beforePosition);
+    const domain = baseEmail.substring(beforePosition);
+    return `${origin}+${alias}${domain}`;
   },
   localeFindByHeader(headers) {
-    if (headers['accept-language'] && headers['accept-language'].includes('ja')) {
-      return 'ja'
+    if (headers["accept-language"] && headers["accept-language"].includes("ja")) {
+      return "ja";
     } else {
-      return 'en'
+      return "en";
     }
   },
   async triggerWebHook(user) {
-    const {ENTRY_CREATE} = strapi.webhookStore.allowedEvents;
-    const modelDef = strapi.getModel('admin::user');
-    const sanitizedEntity = await strapiUtils.sanitize.sanitizers.defaultSanitizeOutput(
-      modelDef,
-      user
-    );
+    const { ENTRY_CREATE } = strapi.webhookStore.allowedEvents;
+    const modelDef = strapi.getModel("admin::user");
+    const sanitizedEntity = await strapiUtils.sanitize.sanitizers.defaultSanitizeOutput(modelDef, user);
     strapi.eventHub.emit(ENTRY_CREATE, {
       model: modelDef.modelName,
       entry: sanitizedEntity,
     });
   },
   triggerSignInSuccess(user) {
-    delete user['password']
-    strapi.eventHub.emit('admin.auth.success', {
+    delete user["password"];
+    strapi.eventHub.emit("admin.auth.success", {
       user,
-      provider: 'strapi-plugin-sso'
+      provider: "strapi-plugin-sso",
     });
   },
   // Sign In Success
   renderSignUpSuccess(jwtToken, user, nonce) {
+    // get REMEMBER_ME from config
+    const config = strapi.config.get("plugin.strapi-plugin-sso");
+    const REMEMBER_ME = config["REMEMBER_ME"];
+
+    let storage = "sessionStorage";
+    if (REMEMBER_ME) {
+      storage = "localStorage";
+    }
+
     return `
 <!doctype html>
 <html>
@@ -83,8 +89,9 @@ module.exports = ({strapi}) => ({
 </noscript>
 <script nonce="${nonce}">
  window.addEventListener('load', function() {
-   sessionStorage.setItem('jwtToken', '"${jwtToken}"');
-   sessionStorage.setItem('userInfo', '${JSON.stringify(user)}');
+
+  ${storage}.setItem('jwtToken', '"${jwtToken}"');
+  ${storage}.setItem('userInfo', '${JSON.stringify(user)}');
    location.href = '${strapi.config.admin.url}'
  })
 </script>
@@ -104,5 +111,5 @@ module.exports = ({strapi}) => ({
 <p>${message}</p>
 </body>
 </html>`;
-  }
+  },
 });
