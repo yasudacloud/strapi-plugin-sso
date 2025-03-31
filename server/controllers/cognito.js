3b3625ae88a6899c2dbabe5d0f8546f7bed65b17
Takenori Nakagawa <ww24@users.noreply.github.com>
2025-04-01 02:04:06 +0900

    fix: Use PKCE in Cognito provider (#82)

--- server/controllers/cognito.js
+++ server/controllers/cognito.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {randomUUID} from 'crypto';
+import pkceChallenge from "pkce-challenge";
 
 const configValidation = () => {
   const config = strapi.config.get('plugin::strapi-plugin-sso')
@@ -29,7 +30,15 @@ async function cognitoSignIn(ctx) {
   const config = configValidation()
   const redirectUri = encodeURIComponent(config['COGNITO_OAUTH_REDIRECT_URI'])
   const endpoint = OAUTH_ENDPOINT(config['COGNITO_OAUTH_DOMAIN'], config['COGNITO_OAUTH_REGION'])
-  const url = `${endpoint}?client_id=${config['COGNITO_OAUTH_CLIENT_ID']}&redirect_uri=${redirectUri}&scope=${OAUTH_SCOPE}&response_type=${OAUTH_RESPONSE_TYPE}`
+
+  // Generate code verifier and code challenge
+  const { code_verifier: codeVerifier, code_challenge: codeChallenge } =
+    pkceChallenge();
+
+  // Store the code verifier in the session
+  ctx.session.codeVerifier = codeVerifier;
+
+  const url = `${endpoint}?client_id=${config['COGNITO_OAUTH_CLIENT_ID']}&redirect_uri=${redirectUri}&scope=${OAUTH_SCOPE}&response_type=${OAUTH_RESPONSE_TYPE}&code_challenge=${codeChallenge}&code_challenge_method=S256`
   ctx.set('Location', url)
   return ctx.send({}, 302)
 }
@@ -52,6 +61,9 @@ async function cognitoSignInCallback(ctx) {
   params.append('client_secret', config['COGNITO_OAUTH_CLIENT_SECRET']);
   params.append('redirect_uri', config['COGNITO_OAUTH_REDIRECT_URI']);
   params.append('grant_type', OAUTH_GRANT_TYPE);
+
+  // Include the code verifier from the session
+  params.append("code_verifier", ctx.session.codeVerifier);
 
   try {
     const tokenEndpoint = OAUTH_TOKEN_ENDPOINT(config['COGNITO_OAUTH_DOMAIN'], config['COGNITO_OAUTH_REGION'])
