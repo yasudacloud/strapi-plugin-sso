606a7d19d5bc316e021be3b914eee824d771557b
Takenori Nakagawa <ww24@users.noreply.github.com>
2025-05-08 23:31:48 +0900

    fix: Add state param in Cognito provider (#88)
    * refactor: Refactor URL construction in Cognito provider
    
    * fix: Add state param in Cognito provider

--- server/controllers/cognito.js
+++ server/controllers/cognito.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {Buffer} from 'buffer';
 import {randomUUID} from 'crypto';
 import pkceChallenge from "pkce-challenge";
 
@@ -23,12 +24,11 @@ const OAUTH_USER_INFO_ENDPOINT = (domain, region) => {
   return `https://${domain}.auth.${region}.amazoncognito.com/oauth2/userInfo`
 }
 const OAUTH_GRANT_TYPE = 'authorization_code'
-const OAUTH_SCOPE = encodeURIComponent('openid email profile')
+const OAUTH_SCOPE = 'openid email profile'
 const OAUTH_RESPONSE_TYPE = 'code'
 
 async function cognitoSignIn(ctx) {
   const config = configValidation()
-  const redirectUri = encodeURIComponent(config['COGNITO_OAUTH_REDIRECT_URI'])
   const endpoint = OAUTH_ENDPOINT(config['COGNITO_OAUTH_DOMAIN'], config['COGNITO_OAUTH_REGION'])
 
   // Generate code verifier and code challenge
@@ -38,7 +38,18 @@ async function cognitoSignIn(ctx) {
   // Store the code verifier in the session
   ctx.session.codeVerifier = codeVerifier;
 
-  const url = `${endpoint}?client_id=${config['COGNITO_OAUTH_CLIENT_ID']}&redirect_uri=${redirectUri}&scope=${OAUTH_SCOPE}&response_type=${OAUTH_RESPONSE_TYPE}&code_challenge=${codeChallenge}&code_challenge_method=S256`
+  const state = crypto.getRandomValues(Buffer.alloc(32)).toString('base64url');
+  ctx.session.oidcState = state;
+
+  const params = new URLSearchParams();
+  params.append('client_id', config['COGNITO_OAUTH_CLIENT_ID']);
+  params.append('redirect_uri', config['COGNITO_OAUTH_REDIRECT_URI']);
+  params.append('scope', OAUTH_SCOPE);
+  params.append('response_type', OAUTH_RESPONSE_TYPE);
+  params.append('code_challenge', codeChallenge);
+  params.append('code_challenge_method', 'S256');
+  params.append('state', state);
+  const url = `${endpoint}?${params.toString()}`
   ctx.set('Location', url)
   return ctx.send({}, 302)
 }
@@ -53,6 +64,9 @@ async function cognitoSignInCallback(ctx) {
 
   if (!ctx.query.code) {
     return ctx.send(oauthService.renderSignUpError(`code Not Found`))
+  }
+  if (!ctx.query.state || ctx.query.state !== ctx.session.oidcState) {
+    return ctx.send(oauthService.renderSignUpError(`Invalid state`))
   }
 
   const params = new URLSearchParams();
